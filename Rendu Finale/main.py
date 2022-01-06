8beac2416d4337f99ec76668edba76a3d8b74926
Alexandre Taesch <alexandre.taesch@etu.utc.fr>
2022-01-06 14:52:19 +0000

    update création de qq requetes rapides - FAIRE FCT

--- Rendu Finale/main.py
+++ Rendu Finale/main.py
@@ -5,6 +5,8 @@ import psycopg2
 conn = psycopg2.connect("dbname='ai23a002' user='me' host='tuxa.sme.utc' password='kZRpoz2T'")
 cur = conn.cursor()
 
+
+"""
 sql = "SELECT num, nom, population FROM dpt2 ORDER BY num";
 cur.execute(sql)
 print ("\t[N°] Nom (Population)")
@@ -24,6 +26,44 @@ sql = "SELECT MIN(population) FROM dpt2";
 cur.execute(sql)
 row = cur.fetchone()
 print("\n> Département le moins peuplé :", row[0]);
+"""
+
+
+def main():
+    cont = 1
+    while cont == 1:
+        menu()
+        cont = int(input("Continuer ? 1 si oui, 0 si non\n> "))
+    conn.close()
+    print(conn)
+    return 0
+
+def menu():
+    print("\nListe requêtes :")
+    print(" 1.   Liste des Parkings")
+    print(" 2.   Liste des Utilisateurs")
+    print(" 3.   Afficher toutes les Places Disponibles")
+    print(" Autre.. TO DO")
+
+    choice = int(input("Choix : "))
+
+    if choice == 1:
+        liste_parking()
+    
+    if choice == 2:
+        liste_users()
+
+    if choice == 3:
+        places_dispo()
+
+# Affiche les infos basiques de chaque parking
+def liste_parking():
+
+# Affiche les infos basiques de chaques users
+def liste_users():
+
+# Récupère en temps réel le nombres de places dispo dans chaque parking (ex 80 places : 14 libres / 52 occupées / 14 réservées)
+def places_dispo():
 
 
 def afficher_voitures_utilsateur(User):
